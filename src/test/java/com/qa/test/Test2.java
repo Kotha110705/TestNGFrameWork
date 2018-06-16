4ce08e3994fa03ba0ee377a0a8387e721fdfa01a
Bhaskar050711 <40012410+Bhaskar050711@users.noreply.github.com>
2018-06-16 13:18:53 +0530

    updated
    updated

---
+++ src/test/java/com/qa/test/Test2.java
@@ -0,0 +1,23 @@
+package com.qa.test;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+
+public class Test2 {
+  @Test
+  public void f() {
+	  
+	  System.out.println("Test2");
+  }
+  @BeforeMethod
+  public void beforeMethod() {
+	  System.out.println("Before Method in t2");
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+	  System.out.println("After Method in t2");
+  }
+
+}
